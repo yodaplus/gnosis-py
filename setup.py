ac44a9a17a3debeaec32f0f1c7eaa2552efc47b9
Taras Yevtushenko <taras.evt@gmail.com>
2021-08-19 11:28:52 +0300

    XinFin mainnet config

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ extras_require = {
 
 setup(
     name='gnosis-yodaplus-py',
-    version='3.1.16',
+    version='3.1.17',
     packages=find_packages(),
     package_data={'gnosis': ['py.typed']},
     install_requires=requirements,
